a569373b4adad7f74c1ae977438a2a50ac90f691
giochi99 <109629800+giochi99@users.noreply.github.com>
2022-12-31 18:35:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
+import pandas as pd
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/api/v1/clothes_dataset', methods=['GET'])
+def get_clothes_dataset():
+    clothes_dataset = pd.read_csv('clothes_dataset.csv')
+    return jsonify(clothes_dataset.to_dict(orient='records'))
+
+# Search for a product in the 'productDisplayName' column
+@app.route('/search/<string:product>', methods=['GET'])
+def get_fuzzy_product(product):
+    clothes_dataset = pd.read_csv('clothes_dataset.csv')
+    product = clothes_dataset[clothes_dataset['productDisplayName'].str.contains(product, case=False)]
+    return jsonify(product.to_dict(orient='records'))  
+
+if __name__ == '__main__':
+    app.run(debug=True)
