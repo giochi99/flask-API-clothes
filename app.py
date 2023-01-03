0d10e7260b6720b275bf99ce5584aa2574997f50
giochi99 <109629800+giochi99@users.noreply.github.com>
2023-01-03 23:47:30 +0100

    Fixed functions'names

--- app.py
+++ app.py
@@ -16,28 +16,28 @@ def get_fuzzy_product(product):
     return jsonify(product.to_dict(orient='records'))  
 
 @app.route('/search/top-wear/<string:product>', methods=['GET'])
-def get_fuzzy_product(product):
+def get_top_wear(product):
     clothes_dataset = pd.read_csv('clothes_dataset.csv')
     top_wear = clothes_dataset[clothes_dataset['subCategory'].isin(["Topwear", "Loungewear and Nightwear", "Apparel Set", "Saree", "Dress"])]
     product = top_wear[top_wear['productDisplayName'].str.contains(product, case=False)]
     return jsonify(product.to_dict(orient='records'))  
 
 @app.route('/search/bottom-wear/<string:product>', methods=['GET'])
-def get_fuzzy_product(product):
+def get_bottom_wear(product):
     clothes_dataset = pd.read_csv('clothes_dataset.csv')
     bottom_wear = clothes_dataset[clothes_dataset['subCategory'].isin(["Bottomwear", "Innerwear", "Socks"])]
     product = bottom_wear[bottom_wear['productDisplayName'].str.contains(product, case=False)]
     return jsonify(product.to_dict(orient='records'))  
 
 @app.route('/search/foot-wear/<string:product>', methods=['GET'])
-def get_fuzzy_product(product):
+def get_foot_wear(product):
     clothes_dataset = pd.read_csv('clothes_dataset.csv')
     foot_wear = clothes_dataset[clothes_dataset['masterCategory'].isin(["Footwear"])]
     product = foot_wear[foot_wear['productDisplayName'].str.contains(product, case=False)]
     return jsonify(product.to_dict(orient='records'))  
 
 @app.route('/search/accessories/<string:product>', methods=['GET'])
-def get_fuzzy_product(product):
+def get_accessories(product):
     clothes_dataset = pd.read_csv('clothes_dataset.csv')
     accessories = clothes_dataset[clothes_dataset['masterCategory'].isin(["Accessories", "Personal Care"])]
     product = accessories[accessories['productDisplayName'].str.contains(product, case=False)]
